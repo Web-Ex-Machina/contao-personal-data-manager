1367ff6e726d18f9ffb6a97e6618d0eb9a18ea1a
ChipsVII <benjamin@webexmachina.fr>
2022-06-15 10:02:53 +0200

    Init repo

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Personal Data Manager for Contao Open Source CMS
+ * Copyright (c) 2015-2022 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-smartgear
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/personal-data-manager/
+ */
+
+namespace WEM\PersonalDataManagerBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use WEM\PersonalDataManagerBundle\WEMPersonalDataManagerBundle;
+
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Web ex Machina <https://www.webexmachina.fr>
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(WEMPersonalDataManagerBundle::class)
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                ])
+                ->setReplace(['wem-personal-data-manager']),
+        ];
+    }
+}
---
+++ src/DependencyInjection/WEMPersonalDataManagerExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Personal Data Manager for Contao Open Source CMS
+ * Copyright (c) 2015-2022 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-smartgear
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/personal-data-manager/
+ */
+
+namespace WEM\PersonalDataManagerBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Adds the bundle services to the container.
+ *
+ * @author Web ex Machina <https://www.webexmachina.fr>
+ */
+class WEMPersonalDataManagerExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $mergedConfig, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('services.yml');
+    }
+}
