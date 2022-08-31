2fc6772c793099f35d58d2b9675fc00a3a397c23
ChipsVII <benjamin@webexmachina.fr>
2022-08-31 10:25:25 +0200

    Fix : export encoding

--- src/Service/PersonalDataManagerAction.php
+++ src/Service/PersonalDataManagerAction.php
@@ -176,7 +176,7 @@ class PersonalDataManagerAction
 
         $csv = $this->manager->exportByPidAndPtableAndEmail(Input::post('pid'), Input::post('ptable'), Input::post('email'));
 
-        (new Response(utf8_decode($csv), 200, [
+        (new Response($csv, 200, [
             'Content-Type' => 'text/csv; charset=utf-8',
             'Content-Disposition' => 'attachment',
             'filename' => $this->translator->trans('WEM.PEDAMA.CSV.filenameSingleItem', [], 'contao_default').'.csv',
@@ -194,7 +194,7 @@ class PersonalDataManagerAction
 
         $csv = $this->manager->exportByEmail(Input::post('email'));
 
-        (new Response(utf8_decode($csv), 200, [
+        (new Response($csv, 200, [
             'Content-Type' => 'text/csv; charset=utf-8',
             'Content-Disposition' => 'attachment',
             'filename' => $this->translator->trans('WEM.PEDAMA.CSV.filenameAll', [], 'contao_default').'.csv',
